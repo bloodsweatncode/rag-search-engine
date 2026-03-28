10f28ec2026a8df88dbdb186bf75796d0b7a2a2c
bloodsweatncode <christian.schmidt@bluedragon.de>
2026-03-28 23:41:29 +0100

    Pre ignoredndex base search

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-from lib.keyword_search import search_command
+from lib.keyword_search import build_command, search_command 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -10,6 +10,7 @@ def main() -> None:
 
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
+    search_parser = subparsers.add_parser("build", help="Build inverted Index and save it")
 
     args = parser.parse_args()
     
@@ -20,6 +21,10 @@ def main() -> None:
             results = search_command(args.query)
             for i, res in enumerate(results, 1):
                 print(f"{i}. {res['title']}")
+        case "build":
+            print("Building inverted index...")
+            build_command()
+            print("Inverted index built successfully.")
         case _:
             parser.print_help()
 
--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,19 +1,82 @@
+import os
 import string
+import pickle
 
-from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies
+from .search_utils import (
+    CACHE_DIR,
+    DEFAULT_SEARCH_LIMIT, 
+    load_movies, 
+    load_stopwords
+)
+
+from nltk.stem import PorterStemmer
+
+from collections import defaultdict
+
+
+class InvertedIndex:
+
+    def __init__(self):
+        self.index = defaultdict(set)
+        self.docmap = {}
+        self.index_path = os.path.join(CACHE_DIR, "index.pkl")
+        self.docmap_path = os.path.join(CACHE_DIR, "docmap.pkl")
+
+    
+    def __add_document(self, doc_id, text):
+        tokenized_text = tokenize_text(text)
+        for token in tokenized_text:
+            self.index[token].add(doc_id)
+
+    def get_documents(self, term):
+        return sorted(self.index.get(term, set()))
+    
+    def build(self):
+        movies = load_movies()
+        for movie in movies:
+            self.docmap[movie['id']] = movie
+            self.__add_document(movie['id'], f"{movie['title']} {movie['description']}")
+
+    def save(self):
+        os.makedirs(CACHE_DIR, exist_ok=True)   
+        with open(self.index_path, "wb") as f:
+            pickle.dump(self.index, f)
+        with open(self.docmap_path, "wb") as f:
+            pickle.dump(self.docmap, f)
+
+    def load(self):
+        with open(self.index_path, "rb") as f:
+            pickle.load(self.index, f)
+        with open(self.docmap_path, "rb") as f:
+            pickle.load(self.docmap, f)
+
+
+def build_command() -> None:
+    idx = InvertedIndex()
+    idx.build()
+    idx.save()
 
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
     results = []
     for movie in movies:
-        preprocessed_query = preprocesse_text(query)
-        preprocessed_title = preprocesse_text(movie["title"])
-        if preprocessed_query in preprocessed_title:
+        query_tokens = tokenize_text(query)
+        title_tokens = tokenize_text(movie["title"])
+        if has_matching_token(query_tokens, title_tokens):
             results.append(movie)
             if len(results) >= limit:
                 break
+    
     return results
+
+
+def has_matching_token(query_tokens: list[str], title_tokens: list[str]) -> bool:
+    for query_token in query_tokens:
+        for title_token in title_tokens:
+            if query_token in title_token:
+                return True
+    return False
 
 
 def preprocesse_text(text: str) -> str:
@@ -21,11 +84,17 @@ def preprocesse_text(text: str) -> str:
     text = text.translate(str.maketrans("", "", string.punctuation))
     return text
 
-def tokenize(input: str) -> list:
-    puncDic = {}
-    for punc in string.punctuation:
-        puncDic[punc] = None
-    puncTrans = str.maketrans(puncDic)
-    output = input.lower().translate(puncTrans)
-    print(output)
-    return ""
+
+def tokenize_text(text: str) -> list[str]:
+    stopwords = load_stopwords()
+    text = preprocesse_text(text)
+    stemmer = PorterStemmer()
+    tokens = text.split()
+    valid_tokens = []
+    for token in tokens:
+        if token and token not in stopwords:
+            token = stemmer.stem(token)
+            valid_tokens.append(token)
+    return valid_tokens
+
+
